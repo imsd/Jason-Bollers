3b269e8f2c50598048bb9d3566b64886171042ce
Student <student@ent-v314-002.local>
2017-09-07 14:27:20 -0400

    move file added

---
+++ Day 1/Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	//public Vector3 myRotation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.Translate(Vector3.forward * Time.deltaTime);
+		}
+
+}
