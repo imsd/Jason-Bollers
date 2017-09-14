10445c2c9b76d7cc7c6b10f93b76569ad894abca
Student <student@ent-v314-002.local>
2017-09-14 13:59:39 -0400

    added new stuff

---
+++ Day 1/Assets/Scripts/Iteration.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iteration : MonoBehaviour {
+
+	public GameObject go;
+	// Use this for initialization
+	void Start () {
+		for (int i = 3; i < 13; i++) {
+			Vector3 myvec = new Vector3 (0, i, 0);
+			Instantiate (go,myvec,Quaternion.identity);
+			
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
