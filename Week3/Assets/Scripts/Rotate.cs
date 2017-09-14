d672472ea28b1c916a0f2a564ba743c1f90dd01d
Student <student@ent-v314-002.local>
2017-09-14 15:51:19 -0400

    Added new project

---
+++ Week3/Assets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// nameing Vector3 to myRotation
+	public Vector3 myRotation;
+	// Use this for initialization
+	//Runs only once
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	//Continues to run
+	void Update () {
+		transform.Rotate (myRotation * Time.deltaTime);
+	}
+}
---
+++ Week3/Assets/Scripts/RotateOnHover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnHover : MonoBehaviour {
+	public Vector3 myRotation;
+	public GameObject go;
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseEnter(){
+		rend.material.color = Color.red;
+	}
+	void OnMouseExit(){
+		rend.material.color = Color.white;
+	}
+	void OnMouseOver(){
+		transform.Rotate (myRotation * Time.deltaTime);
+		for (int i = 1; i < 1; i++) {
+			Vector3 myvec = new Vector3 (0, i, 0);
+			Instantiate (go,myvec,Quaternion.identity);
+
+		}
+	}
+}
