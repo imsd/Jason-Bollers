cd16324f7f1df44746122a7afe7f64aedc2b6965
Jbollers <jayboy10022@gmail.com>
2017-11-02 14:21:38 -0400

    added project2

--- New Unity Project 1/Assets/RotateDegrees.cs
+++ New Unity Project 1/Assets/RotateDegrees.cs
@@ -22,63 +22,72 @@ public class RotateDegrees : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 //		transform.Rotate (Vector3.up, * (RotationSpeed * Time.deltaTime));
-//		if (Input.GetKeyDown ("space")) {
-//		Application.LoadLevel ("Cube");
-//		}
-			if (rotating1) {
-			Vector3 to = new Vector3 (90, 0, 0);
-			if (Vector3.Distance (transform.eulerAngles, to) > 0.01f) {
-				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to, Time.deltaTime);
-			} else {
-				transform.eulerAngles = to;
-				rotating1 = false;
-			}
-		} else if (rotating2) {
-			Vector3 to2 = new Vector3 (0, 90, 0);
-			if (Vector3.Distance (transform.eulerAngles, to2) > 0.01f) {
-				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to2, Time.deltaTime);
-			} else {
-				transform.eulerAngles = to2;
-				rotating2 = false;
-			}
-			
-		} else if (rotating3) {
-			Vector3 to3 = new Vector3 (0, 0, 360);
-			if (Vector3.Distance (transform.eulerAngles, to3) > 0.01f) {
-				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to3, Time.deltaTime);
-			} else {
-				transform.eulerAngles = to3;
-				rotating3 = false;
-			}
-		}
-		else if (rotating4) {
-			Vector3 to4 = new Vector3 (0, 180, 0);
-			if (Vector3.Distance (transform.eulerAngles, to4) > 0.01f) {
-				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to4, Time.deltaTime);
-			} else {
-				transform.eulerAngles = to4;
-				rotating4 = false;
-			}
-		}
-		else if (rotating5) {
-			Vector3 to5 = new Vector3 (0, 270, 0);
-			if (Vector3.Distance (transform.eulerAngles, to5) > 0.01f) {
-				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to5, Time.deltaTime);
-			} else {
-				transform.eulerAngles = to5;
-				rotating5 = false;
-			}
-		}
-		else if (rotating6) {
-			Vector3 to6 = new Vector3 (90, 180, 0);
-			if (Vector3.Distance (transform.eulerAngles, to6) > 0.01f) {
-				transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to6, Time.deltaTime);
-			} else {
-				transform.eulerAngles = to6;
-				rotating6 = false;
-			}
-		}
+		if (Input.GetKey ("down")) {
 
-			
+		if (rotating1) {
+		Vector3 to = new Vector3 (90, 0, 0);
+		if (Vector3.Distance (transform.eulerAngles, to) > 0.01f) {
+		transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to, Time.deltaTime);
+		} else {
+		transform.eulerAngles = to;
+		rotating1 = false;
+		}
+		}
+		}
+		if (Input.GetKey ("right")) {
+		  if (rotating2) {
+		Vector3 to2 = new Vector3 (0, 90, 0);
+		if (Vector3.Distance (transform.eulerAngles, to2) > 0.01f) {
+		transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to2, Time.deltaTime);
+		} else {
+		transform.eulerAngles = to2;
+		rotating2 = false;
+		}
+		}
+			}
+		if (Input.GetKey ("left")) {
+		if (rotating3) {
+		Vector3 to3 = new Vector3 (0, 0, 360);
+		if (Vector3.Distance (transform.eulerAngles, to3) > 0.01f) {
+		transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to3, Time.deltaTime);
+		} else {
+		transform.eulerAngles = to3;
+		rotating3 = false;
+		}
+		}
+		}
+		if(Input.GetKey("up")){
+		 if (rotating4) {
+		Vector3 to4 = new Vector3 (0, 180, 0);
+		if (Vector3.Distance (transform.eulerAngles, to4) > 0.01f) {
+		transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to4, Time.deltaTime);
+		} else {
+		transform.eulerAngles = to4;
+		rotating4 = false;
+		}
+		}
+		}
+		if (Input.GetKey ("a")) {
+		if (rotating5) {
+		Vector3 to5 = new Vector3 (0, 270, 0);
+		if (Vector3.Distance (transform.eulerAngles, to5) > 0.01f) {
+		transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to5, Time.deltaTime);
+		} else {
+		transform.eulerAngles = to5;
+		rotating5 = false;
+		}
+		}
+		}
+		if (Input.GetKey ("d")) {
+		if (rotating6) {
+		Vector3 to6 = new Vector3 (90, 180, 0);
+		if (Vector3.Distance (transform.eulerAngles, to6) > 0.01f) {
+		transform.eulerAngles = Vector3.Lerp (transform.rotation.eulerAngles, to6, Time.deltaTime);
+		} else {
+		transform.eulerAngles = to6;
+		rotating6 = false;
+		}
+		}
+		}
 }
 }
---
+++ Project 2/Project2.md
@@ -0,0 +1,1 @@
+https://github.com/imsd/Maci-Morris/tree/master/Unity%20Project%202
